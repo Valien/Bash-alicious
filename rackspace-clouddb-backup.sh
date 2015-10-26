8041154402cd03389627ccfa23d790d917eb4aa2
Allen Vailliencourt <chumpmonkey@gmail.com>
2015-10-26 17:22:58 -0400

    More work...auth issues! wee!

--- rackspace-clouddb-backup.sh
+++ rackspace-clouddb-backup.sh
@@ -7,13 +7,14 @@
 # API info -- https://developer.rackspace.com/docs/cloud-databases/v1/developer-guide/#document-general-api-info/authenticate
 
 # Cloud DB Datacenter endpoints
+#ORD="https://ord.databases.api.rackspacecloud.com/v1.0/"
+#DFW="https://dfw.databases.api.rackspacecloud.com/v1.0/"
+#IAD="https://iad.databases.api.rackspacecloud.com/v1.0/"
+#LON="https://lon.databases.api.rackspacecloud.com/v1.0/"
+#SYD="https://syd.databases.api.rackspacecloud.com/v1.0/"
+#HKG="https://hkg.databases.api.rackspacecloud.com/v1.0/"
 
-ORD="https://ord.databases.api.rackspacecloud.com/v1.0/"
-DFW="https://dfw.databases.api.rackspacecloud.com/v1.0/"
-IAD="https://iad.databases.api.rackspacecloud.com/v1.0/"
-LON="https://lon.databases.api.rackspacecloud.com/v1.0/"
-SYD="https://syd.databases.api.rackspacecloud.com/v1.0/"
-HKG="https://hkg.databases.api.rackspacecloud.com/v1.0/"
+DBENDPOINT="databases.api.rackspacecloud.com/v1.0"
 
 ## Check to see if token exists. If so use that one instead of generating a new one
 if [ ! -f /tmp/token.txt ]; then
@@ -50,13 +51,18 @@ fi
 #		IAD: https://iad.databases.api.rackspacecloud.com/v1.0/<your account #>/
 #		DFW: https://dfw.databases.api.rackspacecloud.com/v1.0/<your account #>/
 
+#
+## Functions!
+#
+
 #List instances
 listinstances () {
-	INSTANCES=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.databases.api.rackspacecloud.com/v1.0/$TENANTID/instances" 2>/dev/null ) #| python -m json.tool ) #2>/dev/null ) 
+	INSTANCES=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/instances" 2>/dev/null )
 	DBINSTANCES=$( echo "$INSTANCES" | jq ".instances | map(.id) | tostring" )
 
 	if [[ "$DBINSTANCES" = '"[\"\"]"' ]] || [[ "$DBINSTANCES" = '"[]"' ]]; then
-		echo "There are no database instances in $DC"
+		echo "There are no database instances in $DC. Terminating script. Have a nice day."
+		exit
 	else
 		echo
 		echo "JSON: $DBINSTANCES"
@@ -67,6 +73,43 @@ listinstances () {
 	fi
 }
 
+getinstancedetail () {
+	INSTANCEDETAIL=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/instances/$DBSTRIP" )
+}
+
+# create on demand backup
+ondemandbackup () {
+	#GETLIST=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/backups" )
+	CREATEONDEMANDBACKUP=$( curl -v -XPOST -H "Content-Type: application/json X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/schedules" \
+						-d '{ "backup": {
+						"description": "API Generated Backup",
+						"instance": "'$DBSTRIP'",
+						"name": "API DB Snapshot" 
+						}}' )
+}
+
+# list current schedule(s)
+listschedule () {
+	SHOWSCHEDULE=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/schedules" )
+}
+
+# Create new schedule in this case Every Sunday @ 0400 hours
+createschedule () {
+	echo $DBSTRIP
+	CREATESCHEDULE=$( curl -v -XPOST -H "Content-Type: application/json X-Auth-Token: $AUTHTOKEN" "https://$DC.$DBENDPOINT/$TENANTID/schedules" \
+					-d '{ "schedule": {
+						"action": "backup",
+						"day_of_week": 0,
+						"hour": 4,
+						"instance_id": "'$DBSTRIP'",
+						"minute": 00 
+						}}' )
+		echo "Schedule created successfully!"
+		echo
+		listschedule
+		echo $SHOWSCHEDULE | python -m json.tool
+}
+
 #Account info: 
 read -p "Which DC do you want to query? (ORD, DFW, IAD, LON, SYD, HKG)? " DC
 
@@ -74,35 +117,14 @@ if  [[ "$DC" = "ORD" ]] || [[ "$DC" = "DFW" ]] || [[ "$DC" = "IAD" ]] || [[ "$DC
 	echo
 	echo "Now pulling a list of all Cloud DB instances in $DC"
 	listinstances
+	echo
+	getinstancedetail
+	echo $INSTANCEDETAIL | python -m json.tool
+	echo
 else
 	echo "Not a DC. Terminating!"
 	exit
 fi
-
-
-# list current schedule(s)
-listschedule () {
-	SHOWSCHEDULE=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.databases.api.rackspacecloud.com/v1.0/$TENANTID/schedules" | python -m json.tool )
-}
-
-# Create new schedule
-# Every Sunday @ 0400 hours
-createschedule () {
-	CREATESCHEDULE=$( curl -s -XPOST -H "Content-Type: application/json X-Auth-Token: $AUTHTOKEN" "https://$DC.databases.api.rackspacecloud.com/v1.0/$TENANTID/schedules" \
-					-d '{ "schedule": {
-						"action": "backup",
-						"day_of_week": 0,
-						"hour": 4,
-						"instance_id": "'$DBSTRIP'",
-						"minute": 00 
-						}}')
-		echo "Schedule created successfully!"
-		echo
-		#SHOWSCHEDULE=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.databases.api.rackspacecloud.com/v1.0/$TENANTID/schedules" | python -m json.tool )
-		listschedule
-		echo $SHOWSCHEDULE
-}
-
 
 # Create new backup for instance
 read -p "Do you want to (l)ist schedules or (c)reate a new backup schedule? " OPTION
@@ -110,8 +132,8 @@ read -p "Do you want to (l)ist schedules or (c)reate a new backup schedule? " OP
 case $OPTION in
 	"l")
 		# Get current schedule if available
-		#GETSCHEDULES=$( curl -s -XGET -H "X-Auth-Token: $AUTHTOKEN" "https://$DC.databases.api.rackspacecloud.com/v1.0/$TENANTID/schedules" | python -m json.tool )
 		listschedule
+		echo $SHOWSCHEDULE | python -m json.tool
 		#if [[ $SHOWSCHEDULES = '' ]]; then
 		#	echo "No schedules listed. Creating a new schedule."
 		#	createschedule
@@ -119,6 +141,15 @@ case $OPTION in
 		;;
 	"c")
 		createschedule 
+		listschedule
+		echo "Here is your schedule: "
+		echo
+		echo $SHOWSCHEDULE | python -m json.tool
+		;;
+	"o")
+		# show on demand backups
+		ondemandbackup 
+		echo $CREATEONDEMANDBACKUP | python -m json.tool
 		;;
 	*)
 		echo "Sorry wrong option chosen. Terminating."
